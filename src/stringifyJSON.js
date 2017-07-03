f86db5d45e274d599f5989787e51eb902ffcd25b
jhjaylim <jhjaylim@gmail.com>
2017-07-03 16:15:06 -0700

    Define all cases except obj being object

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,48 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var objType = typeof obj;
+  //ID obj
+  if (objType === 'undefined' || objType === 'function') {
+    return null;
+  }
+  //undefined or function
+    //return null
+  if (objType === 'boolean' || objType === 'number' || objType === 'null') {
+    return obj.toString();
+  } else if (objType === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    
+    var stringifiedArray = obj.map(function(ob) {
+      return stringifyJSON(ob);
+    });
+    return '[' + stringifiedArray.join(',') + ']';
+  
+  } else if (objType === 'object') {  
+
+  }
+  //boolean
+    //return convert boolean to string
+  //number
+    //return convert num to string
+  //string
+    //return "string"
+  //null
+    //return convert null to string
+
+  //array
+    //map each array value into new array with stringifyJSON
+    //join() result array
+    //add square brackets around joined string
+    //return string above
+  //object
+    //iterate through obj
+    //store key value pair as an element arr;
+    //push arr into larger arr
+    //map small arrs into string using join()
+    //join entire obj arr with join(':');
+    //add curly braces
+    //return string
+    
 };
